cf98deb59f2d6c5c5d8751303edfa4a5cdceb290
Tejas Aithal <tejasinsf@gmail.com>
2020-05-29 14:51:27 +0530

    add react

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,10 @@ require("channels");
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+// Support component names relative to this directory:
+var componentRequireContext = require.context("components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
 
 import "bootstrap";
 import "../stylesheets/application";
