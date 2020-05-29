9e29e196aaa6ca7cde520dbfdd07983eedbaab5f
Tejas Aithal <tejasinsf@gmail.com>
2020-05-29 18:54:41 +0530

    add CategoryList and NewCategory

---
+++ app/javascript/components/categories/CategoryList.jsx
@@ -0,0 +1,85 @@
+import React, { Component } from "react";
+import NewCategory from "./NewCategory";
+
+class CategoryList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("/api/v1/categories")
+      .then((response) => response.json())
+      .then((response) => {
+        this.setState({ categories: response });
+      });
+  }
+
+  handleDelete = (id) => {
+    fetch(`/api/v1/categories/${id}`, {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" }
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        alert(response.message);
+      })
+      .then(() => this.componentDidMount());
+  };
+
+  handleSetState = () => {
+    this.componentDidMount();
+  };
+
+  render() {
+    const { categories } = this.state && this.state;
+    return (
+      <div className="my-5">
+        <h2>Category List</h2>
+        <NewCategory action={this.handleSetState} />
+        <table className="table table-striped table-bordered text-center">
+          <thead className="thead-light">
+            <tr>
+              <th>Color</th>
+              <th>Category Name</th>
+              <th>
+                <i className="fas fa-trash-alt"></i>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {categories &&
+              categories.map((category, i) => {
+                return (
+                  <tr key={i}>
+                    <th scope="row">
+                      <button
+                        className="btn px-4 py-2"
+                        style={{ background: `${category.color}` }}
+                      >
+                        {category.color}
+                      </button>
+                    </th>
+                    <td>{category.name}</td>
+                    <td>
+                      <button
+                        type="button"
+                        className="btn btn-link"
+                        onClick={() => this.handleDelete(category.id)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default CategoryList;
---
+++ app/javascript/components/categories/NewCategory.jsx
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+
+class NewCategory extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      color: ""
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { name, color } = this.state;
+
+    if (!name || !color) return alert("Please enter all the fields!");
+
+    if (color.length !== 7)
+      return alert("Please enter a valid Hex Code: #FF0000");
+
+    const category = {
+      name,
+      color
+    };
+
+    fetch("/api/v1/categories", {
+      method: "POST",
+      body: JSON.stringify(category),
+      headers: { "Content-Type": "application/json" }
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        alert(response.message);
+        this.setState({
+          name: "",
+          color: ""
+        });
+      })
+      .then(() => this.props.action())
+      .catch((err) => console.log(err));
+  };
+
+  render() {
+    return (
+      <div className="d-flex justify-content-center mt-4">
+        <h5 className="mr-4 mt-1">Add new Category: </h5>
+        <form className="form-inline">
+          <input
+            type="text"
+            className="form-control mb-2 mr-sm-2 shadow-sm"
+            value={this.state.name}
+            name="name"
+            placeholder="Category Name"
+            onChange={this.handleChange}
+          />
+
+          <input
+            className="form-control mb-2 mr-sm-2 shadow-sm"
+            value={this.state.color}
+            name="color"
+            placeholder="Color: #FF0000"
+            onChange={this.handleChange}
+          />
+
+          <button
+            type="submit"
+            className="btn btn-success mr-2 mb-2"
+            onClick={this.handleSubmit}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default NewCategory;
