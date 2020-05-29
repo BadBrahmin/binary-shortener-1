7b67d98d3a4eadd8110db447da7641a53fe496e0
Tejas Aithal <tejasinsf@gmail.com>
2020-05-29 15:27:47 +0530

    add category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_29_081145) do
+ActiveRecord::Schema.define(version: 2020_05_29_094520) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "color", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_categories_on_name", unique: true
+  end
 
   create_table "links", force: :cascade do |t|
     t.string "original", null: false
@@ -18,7 +26,10 @@ ActiveRecord::Schema.define(version: 2020_05_29_081145) do
     t.boolean "pinned", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_links_on_category_id"
     t.index ["short_hash"], name: "index_links_on_short", unique: true
   end
 
+  add_foreign_key "links", "categories"
 end
