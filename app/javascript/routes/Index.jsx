6136766fd00d1733f54f9b3b1d842ec2615befd6
Tejas Aithal <tejasinsf@gmail.com>
2020-05-29 18:54:41 +0530

    add CategoryList and NewCategory

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Header from "../components/Header";
 import LinkList from "../components/links/LinkList";
-// import CategoryList from "../components/categories/CategoryList";
+import CategoryList from "../components/categories/CategoryList";
 
 export default (
   <Router>
@@ -11,7 +11,7 @@ export default (
     <div className="text-center container mb-6">
       <Switch>
         <Route exact path="/" component={LinkList} />
-        {/* <Route exact path="/category" component={CategoryList} /> */}
+        <Route exact path="/category" component={CategoryList} />
       </Switch>
     </div>
   </Router>
